fdd2b7d46c44f1ce60568225aeb1ebbd3d9216ec
Marcin <polbrat.marcin@gmail.com>
2016-09-09 21:53:32 +0200

    Add sliders and "noLight" icons to TabOswietlenie

--- src/com/marpolbr/TabOswietlenie.java
+++ src/com/marpolbr/TabOswietlenie.java
@@ -4,35 +4,42 @@ import javafx.scene.control.Tab;
 
 import javax.swing.*;
 import java.awt.*;
-
+//TODO:Zrobic zeby ikony zmianialy sie z wartoscia suwaka
 public class TabOswietlenie extends JPanel {
 
-    public static final int xPoczatekOpcji = 0;
+    public static final int xPoczatekOpcji = 20;
 
     // Checkboxy (TODO: jako procenty?)
     public static final int wysokoscCheckboxa  = 20;
     public static final int szerokoscCheckboxa = 120;
-    public static final int yPozycjaCheckboxa  = 0;
-    public static final int yOdstepCheckboxa   = 30;
+    public static final int yPozycjaCheckboxa  = 20;
+    public static final int yOdstepCheckboxa   = 50;
 
     JPanel[] pokoje;
 
-    //Obrazy
+    //Ikony
+    public static final int xRozmiarIkony = 40;
+    public static final int yRozmiarIkony = 40;
     private ImageIcon noLight;
     private ImageIcon lowLight;
     private ImageIcon mediumLight;
     private ImageIcon highLight;
     private ImageIcon maximumLight;
 
-    private JSlider suwakOswietlenia;
+    //Suwaki
+    public static final int dlugoscSuwaka = 300;
+    public static final int minimumSuwakaNatezenia = 0;
+    public static final int maximumSuwakaNatezenia = 100;
+    public static final int defaultSuwakaNatezenia = 0;
 
 
     public TabOswietlenie(JPanel[] pokoje){
         this.pokoje = pokoje;
         this.setLayout(null);
-        dodajListePokoiOswietlenia();
-        ustawNatezenieSwiatla();
         zaladujObrazyNatezenia();
+        dodajListePokoiOswietlenia();
+
+
     }
 
     public void dodajListePokoiOswietlenia() {
@@ -46,27 +53,51 @@ public class TabOswietlenie extends JPanel {
                     szerokoscCheckboxa,
                     wysokoscCheckboxa,
                     pokoje[i]);
+
+            stworzNatezenieSwiatla(
+                    szerokoscCheckboxa+xPoczatekOpcji,
+                    yPozycjaCheckboxa + yOdstepCheckboxa * i,
+                    dlugoscSuwaka,
+                    wysokoscCheckboxa
+            );
+
+            stworzIkoneNatezenia(
+                    szerokoscCheckboxa+xPoczatekOpcji+dlugoscSuwaka,
+                    yOdstepCheckboxa * i,
+                    xRozmiarIkony,
+                    yRozmiarIkony
+            );
         }
     }
 
-//TODO: Checkboxy umieszczone niezgodnie z pozycja absolutna, prawdopodobnie dziala Layout domyslny
     public void stworzCheckboxa(String nazwa, int x1, int y1, int xOkno, int yOkno, JPanel pokoj) {
         JCheckBox checkBox = new JCheckBox(nazwa, false);
         checkBox.setBounds(x1, y1, xOkno, yOkno);
-        checkBox.setBackground(Color.cyan);
+      //checkBox.setBackground(Color.cyan);
         add(checkBox);
         checkBox.addItemListener(new WlacznikSwiatla(pokoj));
     }
 
-    public void ustawNatezenieSwiatla(){
-        suwakOswietlenia = new JSlider(0,150,0);
-        suwakOswietlenia.setBounds(100,100,100,100);
+    public void stworzNatezenieSwiatla(int x1, int y1, int xOkno, int yOkno){
+        JSlider suwakOswietlenia = new JSlider(minimumSuwakaNatezenia,maximumSuwakaNatezenia,defaultSuwakaNatezenia);
+        suwakOswietlenia.setBounds(x1,y1,xOkno,yOkno);
+      //suwakOswietlenia.setBackground(Color.RED);
+        add(suwakOswietlenia);
     }
+
+    public void stworzIkoneNatezenia(int x1, int y1, int xOkno, int yOkno){
+        JLabel ikonaNatezenia = new JLabel();
+        ikonaNatezenia.setIcon(noLight);
+        ikonaNatezenia.setBounds(x1,y1,xOkno,yOkno);
+        add(ikonaNatezenia);
+    }
+
+    //TODO: Nie dziala ladowanie obrazow z src
     public void zaladujObrazyNatezenia(){
-        noLight = new ImageIcon("no-light.png");
-        lowLight = new ImageIcon("low-light.png");
-        mediumLight = new ImageIcon("medium-light.png");
-        highLight = new ImageIcon("high-light.png");
-        maximumLight = new ImageIcon("maximum-light.png");
+        noLight = new ImageIcon("D:\\IdeaProjects\\TheSmartHomeVer2\\src\\images\\small icons\\no-light.png");
+        lowLight = new ImageIcon("D:\\IdeaProjects\\TheSmartHomeVer2\\src\\images\\small icons\\low-light.png");
+        mediumLight = new ImageIcon("D:\\IdeaProjects\\TheSmartHomeVer2\\src\\images\\small icons\\medium-light.png");
+        highLight = new ImageIcon("D:\\IdeaProjects\\TheSmartHomeVer2\\src\\images\\small icons\\high-light.png");
+        maximumLight = new ImageIcon("D:\\IdeaProjects\\TheSmartHomeVer2\\src\\images\\small icons\\maximum-light.png");
     }
 }
--- src/com/marpolbr/TabTemperatura.java
+++ src/com/marpolbr/TabTemperatura.java
@@ -4,5 +4,5 @@ import javax.swing.*;
 
 
 public class TabTemperatura extends JPanel {
-    JSlider suwak = new JSlider(0,150,0);
+
 }
